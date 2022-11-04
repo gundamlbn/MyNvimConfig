e4b427ab7ff4635d0f73f9c0abc28bcb6cda5d12
liubin <bin.liu@haso-soft.com>
2022-11-04 13:51:53 +0800

    first commit

---
+++ ftplugin/java.lua
@@ -0,0 +1,109 @@
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+    -- The command that starts the language server
+    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+    cmd = { -- 💀
+    'java', -- or '/path/to/java17_or_newer/bin/java'
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1', '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product', '-Dlog.protocol=true', '-Dlog.level=ALL', '-Xms1g',
+    '--add-modules=ALL-SYSTEM', '--add-opens', 'java.base/java.util=ALL-UNNAMED', '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED', -- 💀
+    '-jar',
+    'C:/Users/liubin0093/AppData/Local/nvim-data/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+    -- Must point to the                                                     Change this to
+    -- eclipse.jdt.ls installation                                           the actual version
+
+    -- 💀
+    '-configuration', 'C:/Users/liubin0093/AppData/Local/nvim-data/mason/packages/jdtls/config_win', -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+    -- eclipse.jdt.ls installation            Depending on your system.
+    -- 💀
+    -- See `data directory configuration` section in the README
+    '-data', 'C:/Users/liubin0093/AppData/Local/nvim-data/mason/packages/jdtls/workspace/folder'},
+
+    -- 💀
+    -- This is the default if not provided, you can remove it. Or adjust as needed.
+    -- One dedicated LSP server & client will be started per unique root_dir
+    root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+
+    -- Here you can configure eclipse.jdt.ls specific settings
+    -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+    -- for a list of options
+    settings = {
+        java = {}
+    },
+
+    -- Language server `initializationOptions`
+    -- You need to extend the `bundles` with paths to jar files
+    -- if you want to use additional eclipse.jdt.ls plugins.
+    --
+    -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+    --
+    -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+    init_options = {
+        bundles = {}
+    }
+}
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require('jdtls').start_or_attach(config)
+
+-- local current_buff = vim.api.nvim_get_current_buf
+-- -- 在语言服务器附加到当前缓冲区之后
+-- -- 使用 on_attach 函数仅映射以下键
+-- local java_on_attach = function(client, bufnr)
+--     local function buf_set_keymap(...)
+--         vim.api.nvim_buf_set_keymap(bufnr, ...)
+--     end
+--     local function buf_set_option(...)
+--         vim.api.nvim_buf_set_option(bufnr, ...)
+--     end
+
+--     -- Enable completion triggered by <c-x><c-o>
+--     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+--     -- Mappings.
+--     local opts = {
+--         noremap = true,
+--         silent = true
+--     }
+--     -- See `:help vim.lsp.*` for documentation on any of the below functions
+--     buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+--     buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+--     -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+--     buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+--     -- buf_set_keymap('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+--     buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+--     buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+--     buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+--     buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+--     -- 重命名
+--     buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+--     -- 智能提醒，比如：自动导包 已经用lspsaga里的功能替换了
+--     buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+--     buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+--     buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+--     -- buf_set_keymap('n', '<C-j>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+--     buf_set_keymap("n", "<S-C-j>", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+--     buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+--     -- 代码格式化
+--     buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+--     -- buf_set_keymap("n", "<leader>l", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+--     -- buf_set_keymap("n", "<leader>l", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+--     -- 自动导入全部缺失的包，自动删除多余的未用到的包
+--     buf_set_keymap("n", "<A-o>", "<cmd>lua require'jdtls'.organize_imports()<CR>", opts)
+--     -- 引入局部变量的函数 function to introduce a local variable
+--     buf_set_keymap("n", "crv", "<cmd>lua require('jdtls').extract_variable()<CR>", opts)
+--     buf_set_keymap("v", "crv", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
+--     -- function to extract a constant
+--     buf_set_keymap("n", "crc", "<Cmd>lua require('jdtls').extract_constant()<CR>", opts)
+--     buf_set_keymap("v", "crc", "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", opts)
+--     -- 将一段代码提取成一个额外的函数function to extract a block of code into a method
+--     buf_set_keymap("v", "crm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
+
+--     -- 代码保存自动格式化formatting
+--     -- vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+-- end
+
+-- java_on_attach(nil, current_buff)
---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,137 @@
+local overrides = require "custom.plugins.overrides"
+
+return {
+
+    -- 平滑滚动
+    ["karb94/neoscroll.nvim"] = {
+        opt = true,
+        config = function()
+            require "custom.plugins.configs"
+        end,
+        keys = {"<A-j>", "<A-k>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb"}
+    },
+
+    -- 历史修改记录
+    ["mbbill/undotree"] = {
+        opt = true,
+        cmd = "UndotreeToggle"
+    },
+
+    -- 多光标
+    ["mg979/vim-visual-multi"] = {
+        keys = "<C-n>"
+    },
+
+    -- 增删改引号
+    ["tpope/vim-surround"] = {
+        keys = {"c", "d", "S"}
+    },
+
+    -- 快速选中
+    ["gcmt/wildfire.vim"] = {
+        keys = "<CR>"
+    },
+
+    -- 快速分析启动时间
+    ["dstein64/vim-startuptime"] = {
+        cmd = "StartupTime"
+    },
+
+    -- 现代任务系统
+    ["skywind3000/asynctasks.vim"] = {
+        requires = "skywind3000/asyncrun.vim",
+        cmd = {"AsyncTask", "AsyncTaskEdit"},
+        opt = true
+    },
+
+    -- 函数列表
+    ["liuchengxu/vista.vim"] = {
+        cmd = "Vista"
+    },
+
+    -- markdown预览
+    ["iamcco/markdown-preview.nvim"] = {
+        run = "cd app && yarn install",
+        ft = "markdown"
+    },
+
+    -- 制作markdown表格
+    ["dhruvasagar/vim-table-mode"] = {
+        cmd = "TableModeToggle"
+    },
+
+    -- session管理
+    ["rmagatti/auto-session"] = {
+        config = function()
+            require('auto-session').setup {
+                log_level = "error",
+                auto_session_enable_last_session = true
+                -- cwd_change_handling = {
+                --     post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+                --         require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+                --     end
+                -- }
+            }
+        end
+    },
+    ["rmagatti/session-lens"] = {
+        requires = {'rmagatti/auto-session', 'nvim-telescope/telescope.nvim'},
+        after = "telescope.nvim",
+        config = function()
+            require('session-lens').setup({
+                path_display = {'shorten'},
+                previewer = true,
+                prompt_title = 'YEAH SESSIONS'
+            })
+        end
+    },
+
+    -- enables dashboard
+    ["goolord/alpha-nvim"] = {
+        disable = false
+    },
+
+    -- Override plugin definition options
+    ["neovim/nvim-lspconfig"] = {
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.plugins.lspconfig"
+        end
+    },
+
+    ["mfussenegger/nvim-jdtls"] = {
+        ft = "java"
+    },
+    -- overrde plugin configs
+    ["nvim-treesitter/nvim-treesitter"] = {
+        override_options = overrides.treesitter
+    },
+
+    ["williamboman/mason.nvim"] = {
+        override_options = overrides.mason
+    },
+
+    ["kyazdani42/nvim-tree.lua"] = {
+        override_options = overrides.nvimtree
+    },
+
+    -- Install a plugin
+    ["max397574/better-escape.nvim"] = {
+        event = "InsertEnter",
+        config = function()
+            require("better_escape").setup()
+        end
+    },
+
+    -- code formatting, linting etc
+    ["jose-elias-alvarez/null-ls.nvim"] = {
+        after = "nvim-lspconfig",
+        config = function()
+            require "custom.plugins.null-ls"
+        end
+    }
+
+    -- remove plugin
+    -- ["hrsh7th/cmp-path"] = false,
+
+}
